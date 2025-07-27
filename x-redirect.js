55cc87352c623152a35ffc151f6a4964edb31216
Shaan Chander <shaan.chander06@gmail.com>
2025-07-26 20:34:48 -0400

    fixed logic (now more accurate with a regex), added gitignore

--- x-redirect.js
+++ x-redirect.js
@@ -1,5 +1,14 @@
 var anchors = document.getElementsByTagName("a");
+const regex = /\b(?:https?:\/\/)?(?:www\.)?(x\.com|twitter\.com)(\/[^\s]*)?\b/;
 
 for (var i = 0; i < anchors.length; i++) {
-    anchors[i].href = anchors[i].href.replace('x.com', 'xcancel.com').replace('twitter.com', 'xcancel.com')
+    
+    if (anchors[i].href.match(regex)) {
+        console.log('matched url (replacing):', anchors[i].href);
+        
+        // Replace the domain with 'xcancel.com' and keep the path if applicable
+        anchors[i].href = anchors[i].href.replace(regex, (match, domain, path) => {
+            return `https://xcancel.com${path || ''}`;
+        });
+    }
 }
